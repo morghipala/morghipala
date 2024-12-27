d45cc1449df34f5f9f04cd9f2c28353c61e2fd83
morghipala <morghipalazzini@gmail.com>
2024-12-27 03:00:26 +0100

    up

---
+++ Gamify/mediaList.js
@@ -0,0 +1,13 @@
+window.media_list = {
+  categories: {
+    nintendo: {
+      name: "Nintendo",
+      games: {
+        SMO: {
+          name: "Super Mario Odyssey",
+          media: ["Jump Up, Super Star!.wav"],
+        },
+      },
+    },
+  },
+};
