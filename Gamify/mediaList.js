312a92195e92a9b3d97b2f786b19e4b55baac8c1
morghipala <morghipalazzini@gmail.com>
2024-12-27 12:55:59 +0100

    up

--- Gamify/mediaList.js
+++ Gamify/mediaList.js
@@ -5,7 +5,7 @@ window.media_list = {
       games: {
         SMO: {
           name: "Super Mario Odyssey",
-          media: ["Jump Up, Super Star!.wav", "Fossil Falls.wav"],
+          media: ["Jump Up, Super Star!.wav", "Fossil Falls.wav", "New Donk City.wav", "Bowser's Castle.wav"],
         },
         MK8D: {
           name: "Mario Kart 8 (& Deluxe)",
